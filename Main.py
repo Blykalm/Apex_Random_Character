1a08858bf7d4b73e2bbc0dc10f60124ea67bea56
Christian <christianmuller585@gmail.com>
2022-04-03 19:04:40 +0200

    initial commit

---
+++ Main.py
@@ -0,0 +1,24 @@
+import pygame as pg
+import random
+from Gui import Gui
+
+
+
+
+characters = ["Ash", "Bangalore", "Bloodhound", "Caustic",
+
+              "Crypto", "Fuse", "Gibraltar", "Hprizon",
+
+              "Lifeline", "Loba", "Mad Maggie", "Mirage",
+
+              "Octane", "Pathfinder", "Rampart", "Revenant",
+
+              "Seer", "Valkyrie", "Wattson", "Wraith"]
+
+
+
+
+
+
+
+
