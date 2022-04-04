44fbc842119ff9dd3b729e2593db97beb7dbc6b6
Christian <christianmuller585@gmail.com>
2022-04-04 21:56:59 +0200

    Added a basic UI and image

--- Gui.py
+++ Gui.py
@@ -1,7 +1,28 @@
-class Gui:
-    
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
+import tkinter as tk
+from win32api import GetSystemMetrics
+from PIL import ImageTk, Image
 
+class Gui:
+    # Gui constructor
+    def __init__ (self):
+        window = tk.Tk(className="random legend")
+        window.configure(bg="#B93038")
+        
+        screen_width = GetSystemMetrics(0)
+        screen_height = GetSystemMetrics(1)
+        window.geometry(str(round(screen_width*0.5)) + "x" + str(round(screen_height*0.7)))
+        print(str(round(screen_width*0.5)))
+        window.eval('tk::PlaceWindow . center')
+
+        # Image of charecter
+        image_main = Image.open("Images\Characters\Wraith.png")
+        #image_main = img.resize((50, 50), Image.ANTIALIAS)
+        
+        test = ImageTk.PhotoImage(image_main)
+
+        label_image = tk.Label(image= test, height=500, width=500 , bg="#B93038").place(x= 230, y= 50)
+
+
+
+        window.mainloop()
     
