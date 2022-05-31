429cfd844d86fac90bdc3254d6377dbed051c4dc
Christian <christianmuller585@gmail.com>
2022-05-31 21:41:26 +0200

    Setup of APIs

--- backend/Random_Legends/models.py
+++ backend/Random_Legends/models.py
@@ -3,7 +3,8 @@ from django.db import models
 # Create your models here.
 
 class Legend(models.Model):
-    name = models.CharField(max_length=25)
+    name = models.CharField(max_length=25 , default= '0', primary_key= True)
+    legend_class = models.CharField(max_length=25, default='0')
 
     def __str__(self):
         return self.name
--- backend/Random_Legends/views.py
+++ backend/Random_Legends/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import LegendsSerializer
+from .models import Legend
 
 # Create your views here.
+
+class LegendView(viewsets.ModelViewSet):
+    serializer_class = LegendsSerializer
+    queryset = Legend.objects.all()
